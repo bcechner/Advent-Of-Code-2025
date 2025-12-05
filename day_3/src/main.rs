f767793acba0d28d751b1cf020855d3357874af3
Ben Cechner <cechnerb@gmail.com>
2025-12-05 14:26:38 -0500

    Improves day 3 to accept a variable number of digits, which solves part 2

--- day_3/src/main.rs
+++ day_3/src/main.rs
@@ -14,7 +14,7 @@ fn main() {
     for line_result in reader.lines() {
         let line = assert_ok!(line_result);
 
-        max_joltages.push(find_max_joltage(line));
+        max_joltages.push(find_max_joltage(line, 12));
     }
 
     println!("{:?}", max_joltages);
@@ -22,31 +22,47 @@ fn main() {
 }
 
 fn test() {
-    assert!(find_max_joltage("987654321111111".to_string()) == 98);
-    assert!(find_max_joltage("811111111111119".to_string()) == 89);
-    assert!(find_max_joltage("234234234234278".to_string()) == 78);
-    assert!(find_max_joltage("818181911112111".to_string()) == 92);
+    assert!(find_max_joltage("987654321111111".to_string(), 12) == 987654321111);
+    assert!(find_max_joltage("811111111111119".to_string(), 12) == 811111111119);
+    assert!(find_max_joltage("234234234234278".to_string(), 12) == 434234234278);
+    assert!(find_max_joltage("818181911112111".to_string(), 12) == 888911112111);
     
     println!("Test completed! Proceed.");
     println!();
 }
 
-fn find_max_joltage(bank: String) -> i64 {
-    let mut current_highest_joltage = 0;
+fn find_max_joltage(bank: String, number_of_digits: i32) -> i64 {
+    let bank_digits: Vec<i32> = bank.chars().map(|c| c.to_digit(10).unwrap() as i32).collect();
 
-    for i in 0..bank.len() {
-        let candidate_first_digit = &bank[i..i+1];
+    let mut max_joltage_digits: Vec<i32> = Vec::new();
 
-        for j in i+1..bank.len() {
-            let candidate_second_digit = &bank[j..j+1];
+    let mut leftwards_searchable_index = 0;
+    
+    for current_digit in 0..number_of_digits {
+        max_joltage_digits.push(0);
 
-            let candidate_joltage = assert_ok!((candidate_first_digit.to_string() + candidate_second_digit).parse::<i64>());
+        println!("Evaluating digit {}", current_digit);
 
-            if candidate_joltage > current_highest_joltage {
-                current_highest_joltage = candidate_joltage;
+        for current_position in (leftwards_searchable_index..(bank_digits.len() - (number_of_digits - current_digit - 1) as usize) as i32).rev() {
+            println!("Considering position {0}, with value {1}", current_position, bank_digits[current_position as usize]);
+
+            if bank_digits[current_position as usize] >= max_joltage_digits[current_digit as usize] {
+                println!("Found new highest {}", bank_digits[current_position as usize]);
+                max_joltage_digits[current_digit as usize] = bank_digits[current_position as usize];
+
+                leftwards_searchable_index = current_position + 1;
             }
         }
     }
 
-    return current_highest_joltage;
+    let max_joltage = max_joltage_digits.iter()
+                             .map(|digit| digit.to_string())
+                             .collect::<Vec<String>>()
+                             .join("")
+                             .parse()
+                             .unwrap();
+
+    println!("Max joltage: {max_joltage}");
+
+    max_joltage
 }
